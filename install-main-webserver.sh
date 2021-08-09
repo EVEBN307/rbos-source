b54ae078b4ca0feddf270640e391c7bd34032c46
EVEBN307 <69747775+EVEBN307@users.noreply.github.com>
2021-08-09 11:57:08 +0800

    Update install-main-webserver.sh

--- install-main-webserver.sh
+++ install-main-webserver.sh
@@ -1,5 +1,5 @@
-sudo apt-get install apache2
-sudo apt-get install phython3
+sudo apt-get install apache2 -y
+sudo apt-get install phython3 -y
 #sudo apt-get install phython3-*
-sudo apt-get install nodejs
-sudo apt-get install traceroute
+sudo apt-get install nodejs -y
+sudo apt-get install traceroute -y
